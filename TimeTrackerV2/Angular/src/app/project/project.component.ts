ab48ce8567a5a6f925aaf00bba5c9378d7fe7f15
Ashley Stuart <ashleystuart@mail.weber.edu>
2022-10-13 14:06:59 -0600

    Implemented group creation + list in project page

--- TimeTrackerV2/Angular/src/app/project/project.component.ts
+++ TimeTrackerV2/Angular/src/app/project/project.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Router} from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { HttpService } from '../services/http.service';
+import { ICourse } from '../interfaces/ICourse';
+import {IUser} from "../interfaces/IUser";
+import {IGroup} from "../interfaces/IGroup";
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-project',
@@ -13,10 +19,24 @@ export class ProjectComponent implements OnInit {
   private item;
   public projectName;
   public projectDescription;
+  public userTypeHolder: IUser;
+  public groups: IGroup[] = [];
+  
+  public user: any = JSON.parse(localStorage.getItem('currentUser') as string);
+
+  // reveals the create group form
+  public bvis = false;
+
+  //Allow course creation if the user is an Instructor
+  public isInstructor: boolean = false;
 
   constructor(
     private http: HttpClient,
+    private formBuilder: FormBuilder,
     private router: Router,
+    private httpService: HttpService,
+
+    
   ) {
     this.item = localStorage.getItem('currentProject');
     console.log("The current project is: " + this.item);
@@ -25,23 +45,85 @@ export class ProjectComponent implements OnInit {
       this.projectName = this.item[0];
       this.projectDescription = this.item[3];
     }
+
+    this.userTypeHolder = new class implements IUser
+    {
+      firstName?: string;
+      id?: number;
+      isActive?: boolean;
+      lastName?: string;
+      password?: string;
+      salt?: string;
+      type?: string;
+      username?: string;
+    }
   }
+
+  
+
+  //Forms for creating a new group
+  groupForm = this.formBuilder.group({
+    groupName: '',
+  });
 
   ngOnInit(): void {
+    this.httpService.getGroups(1).subscribe((_groups: any) => { this.groups = _groups });
+
+
+    let payload = {
+      username: this.user.username,
+    }
+    //Gets user from database
+    this.httpService.getUser(payload).subscribe((_user: any) =>
+    {
+      this.userTypeHolder = _user
+      //Allow user to create courses if they are an instructor
+      if(this.userTypeHolder.type == "Instructor")
+      {
+        this.isInstructor = true;
+      }
+      else
+      {
+        this.isInstructor = false;
+      }
+    });
   }
 
+    // changes the value of bvis to show the hidden form
+    revealForm(): void {
+      this.bvis = true;
+    }
+  
+    // hide form when clicking cancel?
+    hideForm(): void {
+      this.bvis = false; // set to false
+      //location.reload(); // refresh the page
+    }
+
   createGroup(): void {
+// need to get the current project...
+
+// gets the values from the form,
+    // the instructor ID is the current user ID,
+    // is active is automatically set to true
     let payload = {
+      groupName: this.groupForm.value['groupName'],
+      isActive: true,
+      projectID: 1, // PLACEHOLDER...
+    }
+
+    /*let payload = {
       groupName: 'New Group',
       isActive: true,
     }
-    console.log(payload);
+    console.log(payload);*/
 
     this.http.post<any>('http://localhost:8080/createGroup/', payload, { headers: new HttpHeaders({ "Access-Control-Allow-Headers": "Content-Type" }) }).subscribe({
       next: data => {
         this.errMsg = "";
         localStorage.setItem('currentGroup', JSON.stringify(data['group']));
-        this.router.navigate(['./group']);
+        //this.router.navigate(['./group']);
+        this.groupForm.reset(); //Clears the form data after submitting the data.
       },
       error: error => {
         this.errMsg = error['error']['message'];
