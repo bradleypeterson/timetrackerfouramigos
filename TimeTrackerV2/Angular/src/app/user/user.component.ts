666aa7b84213a36b9874c3c2e60ff4c47f06c838
Ashley Stuart <ashleystuart@mail.weber.edu>
2022-11-17 10:03:02 -0700

    Added user name change functionality

--- TimeTrackerV2/Angular/src/app/user/user.component.ts
+++ TimeTrackerV2/Angular/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminModalService } from '../services/adminmodal.service';
 import { IUser } from '../interfaces/IUser';
 
 @Component({
@@ -10,11 +11,36 @@ export class UserComponent implements OnInit {
 
   user: IUser = JSON.parse(localStorage.getItem('currentUser') as string);
 
-  constructor() { }
+  constructor(
+    public modalService: AdminModalService,
+  ) { }
 
   ngOnInit(): void {
+    
   }
 
   public pageTitle = 'TimeTrackerV2 | User'
+
+
+  changeName() {
+    let firstName = ((document.getElementById("firstname") as HTMLInputElement).value);
+    let lastName = ((document.getElementById("lastname") as HTMLInputElement).value);
+    let userName = ((document.getElementById("username") as HTMLInputElement).value);
+
+    if (firstName != "") {
+      this.user.firstName = firstName;
+    }
+
+    if (lastName != "") {
+      this.user.lastName = lastName;
+    }
+
+    if (userName != "") {
+      this.user.username = userName;
+    }
+
+    this.modalService.updateUser(this.user);
+
+  }
 
 }
