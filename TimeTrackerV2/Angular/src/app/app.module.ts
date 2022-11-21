f493b8a57e05fe09ecebd86a5b626db440c15ab9
DenverJensen <jensendenver@gmail.com>
2022-11-20 17:34:24 -0700

    auth protection on get users route

--- TimeTrackerV2/Angular/src/app/app.module.ts
+++ TimeTrackerV2/Angular/src/app/app.module.ts
@@ -6,25 +6,27 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
-import { NgxChartsModule} from "@swimlane/ngx-charts";
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
 import { RegisterComponent } from './register/register.component';
 import { InstructorComponent } from './instructor/instructor.component';
 import { GroupsComponent } from './groups/groups.component';
-import { AdminModalComponent } from './modals/admin-modal/admin-modal.component';
+import { AdminModalComponent } from './Modals/admin-modal/admin-modal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatTableModule} from "@angular/material/table";
-import {MatRippleModule} from "@angular/material/core";
-import {MatDialogModule} from "@angular/material/dialog";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { EditTimeDialogComponent } from './Modals/edit-time-dialog/edit-time-dialog.component';
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { ChartsComponent } from './group/charts/charts.component';
 import { ResetPassComponent } from './login/reset-pass/reset-pass.component';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -39,26 +41,32 @@ import { ResetPassComponent } from './login/reset-pass/reset-pass.component';
     EditTimeDialogComponent,
     ChartsComponent,
     ResetPassComponent,
-
   ],
   entryComponents: [EditTimeDialogComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatTableModule,
-        MatRippleModule,
-        NgxChartsModule,
-        MatDialogModule,
-        MatInputModule,
-        MatSelectModule
-    ],
-  providers: [FormBuilder],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatRippleModule,
+    NgxChartsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
+  providers: [
+    FormBuilder,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
